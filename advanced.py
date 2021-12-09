e95fa399582590a3ac84ea8f7a50fb83082d412c
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-12-09 20:18:36 +0200

    HW
    # HW1: define "avg(a,b,c)"---> print the average
    # call the function

---
+++ advanced.py
@@ -0,0 +1,14 @@
+# mathematical functions
+a = 10
+b = 20
+c = 30
+def sum(a,b,c):
+    r = a + b + c
+    print(r)
+def avg(a,b,c):
+    r = (a + b + c) / 3
+    print(r)
+# HW1: define "avg(a,b,c)"---> print the average
+# call the function
+sum(10,20,30)
+avg(10,20,30)
